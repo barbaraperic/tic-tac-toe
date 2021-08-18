28c6423d2c9231b74fd75873c7d45868d8be24b7
Barbara Peric <b.peric10@gmail.com>
2021-08-18 10:55:29 +0200

    init function

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,55 @@ const app = document.getElementById('app')
 const boardElement = document.getElementById('board')
 const resetButton = document.getElementById('reset')
 
+// initialize variables
+const ROW_COUNT = 3;
+const COL_COUNT = 3;
 
+type Cell = "X" | "O" | ""
+
+type TicTacToeBoard = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell]
+]
+
+const boardState: TicTacToeBoard = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]
 
 // create cell
+
+const createCell = (row: number, col: number, content: Cell) => {
+  const cell = document.createElement('button');
+  cell.setAttribute('data-row', row.toString())
+  cell.setAttribute('data-col', col.toString())
+  cell.setAttribute('data-content', content)
+  cell.classList.add('cell')
+  return cell
+}
+
+// render board
+
+const renderBoard = () => {
+  if(!boardElement) throw new Error('No board element')
+  boardElement.innerHTML = '';
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COL_COUNT; j++) {
+      boardElement.appendChild(createCell(i, j, boardState[i][j]))
+    }
+  }
+}
+
+
+const init = () => {
+  renderBoard()
+}
+
+init()
+
+
+
 
 
