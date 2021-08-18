86bde779877f8d47eacc66ad6f45c0da463d8e1e
Barbara Peric <b.peric10@gmail.com>
2021-08-18 10:45:33 +0200

    init typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const app = document.getElementById('app')
+const boardElement = document.getElementById('board')
+const resetButton = document.getElementById('reset')
+
+
+
+// create cell
+
+
